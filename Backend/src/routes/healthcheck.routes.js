0296ee3a9294b67633a37eea7a80400568b2a5e7
praveen <palpraveen3221@gmail.com>
2025-04-23 00:20:13 +0530

    feat: setup Prism Client

--- Backend/src/routes/healthcheck.routes.js
+++ Backend/src/routes/healthcheck.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { healthCheck } from "../controllers/healthcheck.controller";
+import { healthCheck } from "../controllers/healthcheck.controller.js";
 
 const router=Router();
 
