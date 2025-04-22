82fbcb8547f9a04ccc9192de486b4a1fa474dd49
praveen <palpraveen3221@gmail.com>
2025-04-22 19:39:34 +0530

    feat: setup app and index logic,with dotenv

---
+++ Backend/src/index.js
@@ -0,0 +1,14 @@
+import app from "./app.js"
+import dotenv from "dotenv"
+
+
+dotenv.config();
+
+const port= process.env.PORT || 4000
+
+
+app.listen(port,()=>{
+    console.log("App is listening")
+});
+
+
