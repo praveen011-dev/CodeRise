bc4f58fba960b2d5e7a09cbade90e984eb0a28e2
praveen <palpraveen3221@gmail.com>
2025-04-24 14:04:28 +0530

    feat: setup auth controller and add golbal error handler

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,26 @@ app.use(cookieParser());
 //routers imports
 
 import healthCheckRouter from "./routes/healthcheck.routes.js"
+import authRoutes from "./routes/auth.routes.js"
 
 app.use("/api/v1/healthcheck",healthCheckRouter);
+app.use("/api/v1/users",authRoutes);
+
+
+
+// Global express error handler
+
+app.use((err, _req, res, _next) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message: err.message || "Internal Server Error",
+      errors: err.errors || [],
+      stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+    });
+  });
+
+  
 
 export default app
---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const register=asyncHandler(async(req,res)=>{
+
+})
+
+
+export {register}
