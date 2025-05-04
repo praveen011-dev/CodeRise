a21ca82c8af0afd39609a60163909ed576956db7
praveen <palpraveen3221@gmail.com>
2025-05-04 19:34:22 +0530

    fix :resolve some bugs in the problem controller

--- Backend/src/controllers/problem.controller.js
+++ Backend/src/controllers/problem.controller.js
@@ -6,63 +6,84 @@ import {db} from "../libs/db.js"
 
 const createProblem=asyncHandler(async(req,res,next)=>{
 
-    const {title,description,difficulty,tags,examples,constraints,testcases,codeSnippet,refrenceSolution}=req.body
+    const {title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testcases,
+        codeSnippet,
+        refrenceSolution
+        }=req.body
+
+        if (req.user.role !== "ADMIN") {
+            return next(new ApiError(403, "Access Denied!"));
+          }
+
 
     //loop through each refrence solution for different languaes 
+    
+            
     for(const [language,solutionCode] of Object.entries(refrenceSolution)){
+            try{
+                const languageId= getJudge0LanguageId(language);
+        
+                console.log(`Language: ${language}, Language ID: ${languageId}`);
+        
+                if(!languageId){
+                    return next (new ApiError(400,`Invalid Language ${language}`))
+                }
+        
+        
+                const submissions=testcases.map(({input,output})=>{
+                    return {
+                        source_code:solutionCode,
+                        language_id:languageId,
+                        stdin:input,
+                        expected:output
+                    }
+                })
 
-        const languageId= getJudge0LanguageId(language);
 
-        if(!langaugeId){
-            return next (new ApiError(400,`Invalid Language ${language}`))
-        }
-
-        const submissions=testcases.map(({input,output})=>{
-            return {
-                source_code:solutionCode,
-                language_id:languageId,
-                stdin:input,
-                expected:output
+                const submissionResults=await submitBatch(submissions)
+                const Tokens=submissionResults.map((res)=>res.token);    
+                const results=await pollBatchResults(Tokens);
+        
+                for(let i=0;i<results.length;i++){
+                    const result = results[i];
+                    console.log("Result-----",result)
+                    if(result.status.id!==3){
+                        return next(new ApiError(400,"Test case failed"));
+                    }
+                }
             }
-        })
-
-        const submissionResults=await submitBatch(submissions)
-
-        const Tokens=submissionResults.map((res)=>res.token);
-
-        const results=await pollBatchResults(Tokens);
-
-        for(let i=0;i<results.length;i++){
-            const result = results[i];
-            console.log("Result-----",result)
-            if(result.status.id!==3){
-                return next(new ApiError(400,"Test case failed"));
+            catch (error) {
+                return next(new ApiError(500, "Error while creating a problem"));
+        
             }
         }
-
-        //save the problem to the database
-
-        const newProblem= await db.problem.create({
-            data:{
-                title,
-                description,
-                difficulty,
-                tags,
-                userId:req.user.id,
-                examples,
-                constraints,
-                testcases,
-                codeSnippet,
-                refrenceSolution
-            } 
-        })
- 
-        return res
-        .status(201)
-        .json(new ApiResponse(201,newProblem,"problem created successfully"));
-    }
-
-    })
+            //save the problem to the database
+        
+                const newProblem= await db.problem.create({
+                    data:{
+                        title,
+                        description,
+                        difficulty,
+                        tags,
+                        userId:req.user.id,
+                        examples,
+                        constraints,
+                        testcases,
+                        codeSnippet,
+                        refrenceSolution
+                    } 
+                })
+            
+                return res
+                .status(201)
+                .json(new ApiResponse(201,newProblem,"problem created successfully"));
+            })
 
 const getAllProblems=asyncHandler(async(req,res,next)=>{
 
