22632f93f12f3cbce691ee7a0b8ac2317d7c7422
praveen <palpraveen3221@gmail.com>
2025-05-21 23:28:50 +0530

    configure  tailwind v4

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcssVite from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url"; 
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcssVite()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      // Or if you used the 'path' module:
+      // '@': path.resolve(__dirname, './src')
+    },
+  },
+});
