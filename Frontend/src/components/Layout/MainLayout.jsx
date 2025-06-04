dc4b0e415c68e59120e31551e5d0840fa0ecebaa
praveen <palpraveen3221@gmail.com>
2025-06-05 00:39:05 +0530

    Fix path case issue for Footer

--- Frontend/src/components/Layout/MainLayout.jsx
+++ Frontend/src/components/Layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import Footer from "./footer";
+import Footer from "./Footer";
 import useAuthStore from "../../store/authStore";
 import { Toaster } from "@/components/ui/sonner";
 
