cfc8ae3bac5313d9472c77796cc3dde3c0725f6d
praveen <palpraveen3221@gmail.com>
2025-06-05 00:27:38 +0530

    Fix path case issue for Navbar

--- Frontend/src/components/Layout/MainLayout.jsx
+++ Frontend/src/components/Layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import Footer from "./footer";
 import useAuthStore from "../../store/authStore";
 import { Toaster } from "@/components/ui/sonner";
